2b2c90a65ca930d316a0fcfe5b72ee4e68eddfa9
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2019-01-17 23:24:56 +0300

    ExtractMethod kod örneği eklendi

---
+++ Samples/dotnet/RefactoringTechniques/RefactoringTechniques/Refactorings/ExtractMethod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RefactoringTechniques.Refactorings
+{
+    // BAD
+    public class A
+    {
+        public void DoSomeThing()
+        {
+            // some code blocks...
+
+            // write user information to console
+            Console.WriteLine("Kullanıcı adı: ali_veli");
+            Console.WriteLine("E-posta: ali_veli@mail.com");
+        }
+    }
+
+    // GOOD
+    public class B
+    {
+        public void DoSomeThing()
+        {
+            // some code blocks...
+
+            WriteUserInformationToConsole();
+        }
+
+        private static void WriteUserInformationToConsole()
+        {
+            Console.WriteLine("Kullanıcı adı: ali_veli");
+            Console.WriteLine("E-posta: ali_veli@mail.com");
+        }
+    }
+}
