a59989d5a0ca7cc710ae4617e10dd7ddc97faf98
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2019-03-13 20:24:28 +0300

    Create InlineTemp.cs

---
+++ Samples/dotnet/RefactoringTechniques/RefactoringTechniques/Refactorings/InlineTemp.cs
@@ -0,0 +1,22 @@
+﻿namespace RefactoringTechniques.Refactorings
+{
+    // KÖTÜ TASARIM
+    public class InlineTempBad
+    {
+       public int CalculateVolume(int l, int w, int h)
+       {
+           var volume = l * w * h;
+
+           return volume;
+       }
+    }
+
+    // İYİ TASARIM
+    public class InlineTempGood
+    {
+        public int CalculateVolume(int l, int w, int h)
+        {
+            return l * w * h;
+        }
+    }
+}
